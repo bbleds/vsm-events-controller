255217d1d2b7c9c6538265d7ca96829ab77c7d24
Ben Bledsoe <bledsoedesigns@gmail.com>
2017-05-01 21:03:48 -0500

    Added support for handling valid api keys

--- api/index.php
+++ api/index.php
@@ -5,19 +5,18 @@ $config = require_once('../config.php');
 
 // get route passed in
 $query_params = $_GET;
-$action = $query_params['action'];
+$action = isset($_POST['action']) ? $_POST['action'] : $query_params['action'];
 $error = array('msg'=>'API method not found.', 'error'=>1);
 $api = new EventsAPI($config);
 
 // replace dash characters with underscores
 $action =  preg_replace('/(-)/', '_', $action);
 
-print(json_encode($_POST));
-
 // check api key if post
 if($_POST){
   if($_POST['apikey'] !== $config->apikey){
-    return print($_POST['apikey']);
+    $error['msg'] = 'Invalid API key, please obtain a valid API key.';
+    return print(json_encode($error));
   }
 }
 
